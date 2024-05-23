27261ab6bfa1a3992d7aeb7c5c0ea4f302a69b1b
ElliotBruno <elliot.bruno@skola.taby.se>
2024-05-23 10:17:31 +0200

    Last Commit

--- Assets/Scripts/Finished level.cs
+++ Assets/Scripts/Finished level.cs
@@ -29,12 +29,12 @@ public class Finishedlevel : MonoBehaviour
     public int points = 8;
 
     [SerializeField] private TextMeshProUGUI Kiwitext;
-  
 
 
 
 
-void Start()
+
+    void Start()
     {
         finishSound = GetComponent<AudioSource>();
         anim = GetComponent<Animator>();
@@ -44,88 +44,89 @@ void Start()
 
 
 
-        void Update()
+    void Update()
+    {
+
+        //Debug.Log(enemyCount);
+        //Debug.Log(levelCompleted);
+        //Debug.Log(points);
+        if (enemyCount == 0 && points == 0 && levelCompleted != true)
         {
+            anim.SetTrigger("finished");
+            finishSound.Play();
+            levelCompleted = true;
+            Debug.Log("finish");
 
-            //Debug.Log(enemyCount);
-            //Debug.Log(levelCompleted);
-            //Debug.Log(points);
-            if (enemyCount == 0 && points == 0 && levelCompleted != true)
-            {
-                anim.SetTrigger("finished");
-                finishSound.Play();
-                levelCompleted = true;
-                Debug.Log("finish");
+            Invoke("LoadNxtLevel", 1f);
 
-                Invoke("LoadNxtLevel", 1f);
-
-            }
         }
-    /*  void Update()
-     {
-         // Check if enemyCount and points are 0, and if the level is not already completed
-         if (enemyCount == 0 && points == 0 && !levelCompleted)
-         {
-             // Trigger animation
-             if (anim != null)
-                 anim.SetTrigger("finished");
+    }
+    /* void Update()
+    {
+        // Check if enemyCount and points are 0, and if the level is not already completed
+        if (enemyCount == 0 && points == 0 && !levelCompleted)
+        {
+            // Trigger animation
+            if (anim != null)
+                anim.SetTrigger("finished");
 
-             // Play finish sound
-             if (finishSound != null)
-                 finishSound.Play();
+            // Play finish sound
+            if (finishSound != null)
+                finishSound.Play();
 
-             // Mark level as completed
-             levelCompleted = true;
+            // Mark level as completed
+            levelCompleted = true;
 
-             // Log message
-             Debug.Log("Level completed");
+            // Log message
+            Debug.Log("Level completed");
 
-             // Load next level after a delay
-             Invoke("LoadNxtLevel", 1f);
-         }
-     }*/
+            // Load next level after a delay
+            Invoke("LoadNxtLevel", 1f);
+        }
+    }
+    */
+    void CompleteLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
 
-    /* void CompleteLevel()
-     {
-         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
 
-     }*/
-
-    /*public void LoadNextLevel()
+    public void LoadNextLevel()
     {
         // Get the index of the current active scene
         int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
 
         // Load the next scene, assuming it's the next scene in the build settings
         SceneManager.LoadScene(currentSceneIndex + 1);
-    }*/
-    public void LoadNextLevel()
-    {
-        // Get the index of the current active scene
-        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
-
-        // Ensure there is a next scene to load
-        if (currentSceneIndex + 1 < SceneManager.sceneCountInBuildSettings)
-        {
-            // Load the next scene
-            Debug.Log("byt scen");
-            SceneManager.LoadScene(currentSceneIndex + 1);
-        }
-        else
-        {
-            // Output a message indicating that there are no more scenes to load
-            Debug.LogWarning("No more scenes to load. This might be the last level.");
-        }
     }
+    /*   public void LoadNextLevel()
+      {
+          // Get the index of the current active scene
+          int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
 
-    public void LoadNxtLevel()
-    {
-        int nextSceneIndex = SceneManager.GetActiveScene().buildIndex + 1;
-        if (SceneManager.sceneCountInBuildSettings > nextSceneIndex)
-        {
-            SceneManager.LoadScene(nextSceneIndex);
-        }
-    }
+          // Ensure there is a next scene to load
+          if (currentSceneIndex + 1 < SceneManager.sceneCountInBuildSettings)
+          {
+              // Load the next scene
+              Debug.Log("byt scen");
+              SceneManager.LoadScene(currentSceneIndex + 1);
+          }
+          else
+          {
+              // Output a message indicating that there are no more scenes to load
+              Debug.LogWarning("No more scenes to load. This might be the last level.");
+          }
+      }
+  */
+    /* public void LoadNxtLevel()
+     {
+         int nextSceneIndex = SceneManager.GetActiveScene().buildIndex + 1;
+         if (SceneManager.sceneCountInBuildSettings > nextSceneIndex)
+         {
+             SceneManager.LoadScene(nextSceneIndex);
+         }
+     } 
+ */
     private void OnTriggerEnter2D(Collider2D collision)
     {
 
@@ -146,8 +147,8 @@ void Start()
         animator.SetBool("IsDead", true);
         Debug.Log("Died");
         GetComponent<Collider2D>().enabled = false;
-/*        this.enabled = false;
-*/        
+        this.enabled = false;
+
         enemyCount -= 1;
         Enemytext.text = "Enemeis remaining: " + enemyCount;
 
@@ -157,3 +158,82 @@ void Start()
 
 }
 
+
+
+
+/*
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class LevelManager : MonoBehaviour
+{
+    public Animator animator;
+    public AudioSource finishSound;
+    public GameObject player;
+    public TextMeshProUGUI enemyText;
+    public TextMeshProUGUI kiwiText;
+
+    private bool levelCompleted = false;
+    private int enemyCount = 1;
+    private int points = 8;
+
+    private void Start()
+    {
+        finishSound = GetComponent<AudioSource>();
+    }
+
+    private void Update()
+    {
+        if (!levelCompleted && enemyCount == 0 && points == 0)
+        {
+            FinishLevel();
+        }
+    }
+
+    private void FinishLevel()
+    {
+        if (animator != null)
+            animator.SetTrigger("finished");
+
+        if (finishSound != null)
+            finishSound.Play();
+
+        levelCompleted = true;
+        Debug.Log("Level completed");
+
+        Invoke("LoadNextLevel", 1f);
+    }
+
+    private void LoadNextLevel()
+    {
+        int nextSceneIndex = SceneManager.GetActiveScene().buildIndex + 1;
+        if (nextSceneIndex < SceneManager.sceneCountInBuildSettings)
+        {
+            SceneManager.LoadScene(nextSceneIndex);
+        }
+        else
+        {
+            Debug.LogWarning("No more scenes to load. This might be the last level.");
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Kiwi"))
+        {
+            points--;
+            Destroy(collision.gameObject);
+            kiwiText.text = "Fruits remaining: " + points;
+        }
+    }
+
+    public void Die()
+    {
+        enemyCount--;
+        animator.SetBool("IsDead", true);
+        GetComponent<Collider2D>().enabled = false;
+        enemyText.text = "Enemies remaining: " + enemyCount;
+    }
+}
+*/
